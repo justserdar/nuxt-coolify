11711d30d6dfc94647dc363070d4c8ab996cbd1a
Sandros94 <sandro.circi@digitoolmedia.com>
2024-10-01 03:34:58 +0200

    chore: update playground

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -2,111 +2,104 @@
   <div>
     <div>
       <h2>Coolify Instances</h2>
-      <client-only>
-        <div v-if="cStatus === 'pending'">
-          Loading Instances...
-        </div>
-        <div v-else-if="cError">
-          Error: {{ cError?.message }}
-        </div>
-        <div v-else-if="cStatus === 'success'">
-          <ul style="list-style: none;">
-            <li>
-              Total Instances:: {{ instances?.length }}
-            </li>
-            <li>
-              <button @click.prevent="refreshInstanceList()">
-                Refresh Servers
-              </button>
-            </li>
-          </ul>
-          <div class="grid grid-cols-3 gap-3 py-3">
-            <div
-              v-for="(serverGroup, groupIndex) in instances"
-              :key="groupIndex"
-              class="bg-gray-200 dark:bg-gray-900 p-2 rounded-lg shadow-md"
-            >
-              <div class="grid grid-cols-2 gap-3 space-y-1 px-3 py-4">
+      <div v-if="cStatus === 'pending'">
+        Loading Instances...
+      </div>
+      <div v-else-if="cError">
+        Error: {{ cError?.message }}
+      </div>
+      <div v-else-if="cStatus === 'success'">
+        <ul style="list-style: none;">
+          <li>
+            Total Instances:: {{ instances?.length }}
+          </li>
+          <li>
+            <button @click.prevent="refreshInstanceList()">
+              Refresh Servers
+            </button>
+          </li>
+        </ul>
+        <div class="grid grid-cols-3 gap-3 py-3">
+          <div
+            v-for="(serverGroup, groupIndex) in instances"
+            :key="groupIndex"
+            class="bg-gray-200 dark:bg-gray-900 p-2 rounded-lg shadow-md"
+          >
+            <div class="grid grid-cols-2 gap-3 space-y-1 px-3 py-4">
+              <template
+                v-for="(instanceGroup, instanceGroupIndex) in serverGroup"
+                :key="instanceGroupIndex"
+              >
                 <template
-                  v-for="(instanceGroup, instanceGroupIndex) in serverGroup"
-                  :key="instanceGroupIndex"
+                  v-for="instance in instanceGroup"
+                  :key="instance.uuid"
                 >
-                  <template
-                    v-for="instance in instanceGroup"
-                    :key="instance.uuid"
-                  >
-                    <template v-if="instance.settings.id === 1">
+                  <template v-if="instance.settings.id === 1">
+                    <p
+                      class="col-span-2"
+                    >
+                      Instance:
+                    </p>
+
+                    <div class="col-span-2 p-6 border rounded hover:cursor-pointer dark:hover:bg-gray-400">
                       <p
-                        class="col-span-2"
+                        class="flex text-md w-full"
+                        @click="navigateTo(`/hub/s/${instance.uuid}`)"
                       >
-                        Instance:
+                        <span class="font-bold text-xs mr-2">{{ instance.name }}</span>
+                      </p>
+                      <div
+                        v-if="instance.settings.is_reachable"
+                        class=" bg-green-500 w-2 h-2 my-auto rounded-full animate-pulse"
+                      />
+                      <div
+                        v-else
+                        class=" bg-red-500 w-2 h-2 my-auto rounded-full animate-pulse"
+                      />
+                      <span class="font-bold text-xs dark:text-gray-600 ml-auto mr-0">{{ instance.uuid }}</span>
+                    </div>
+                  </template>
+                  <template v-else>
+                    <p
+                      class="col-span-2"
+                    >
+                      Attached Servers:
+                    </p>
+                    <div class="col-span-1 p-6 border rounded">
+                      <p
+                        class="flex text-md"
+                        @click="navigateTo(`/hub/s/${instance.uuid}`)"
+                      >
+                        <span class="font-bold text-xs mr-2">{{ instance.name }}</span>
                       </p>
 
-                      <div class="col-span-2 p-6 border rounded hover:cursor-pointer dark:hover:bg-gray-400">
-                        <p
-                          class="flex text-md w-full"
-                          @click="navigateTo(`/hub/s/${instance.uuid}`)"
-                        >
-                          <span class="font-bold text-xs mr-2">{{ instance.name }}</span>
-                        </p>
-                        <div
-                          v-if="instance.settings.is_reachable"
-                          class=" bg-green-500 w-2 h-2 my-auto rounded-full animate-pulse"
-                        />
-                        <div
-                          v-else
-                          class=" bg-red-500 w-2 h-2 my-auto rounded-full animate-pulse"
-                        />
-                        <span class="font-bold text-xs dark:text-gray-600 ml-auto mr-0">{{ instance.uuid }}</span>
+                      <div
+                        v-if="instance.settings.is_reachable"
+                        class=" bg-green-500 w-2 h-2 my-auto rounded-full animate-pulse"
+                      />
+                      <div
+                        v-else
+                        class=" bg-red-500 w-2 h-2 my-auto rounded-full animate-pulse"
+                      />
+
+                      <span class="font-bold text-xs dark:text-gray-600 ml-auto mr-0">{{ instance.uuid }}</span>
+
+                      <div class="flex mt-4">
+                        <span class="text-xs dark:text-gray-500">
+                          {{ instance.ip }}
+                        </span>
                       </div>
-                    </template>
-                    <template v-else>
-                      <p
-                        class="col-span-2"
-                      >
-                        Attached Servers:
-                      </p>
-                      <div class="col-span-1 p-6 border rounded">
-                        <p
-                          class="flex text-md"
-                          @click="navigateTo(`/hub/s/${instance.uuid}`)"
-                        >
-                          <span class="font-bold text-xs mr-2">{{ instance.name }}</span>
-                        </p>
-
-                        <div
-                          v-if="instance.settings.is_reachable"
-                          class=" bg-green-500 w-2 h-2 my-auto rounded-full animate-pulse"
-                        />
-                        <div
-                          v-else
-                          class=" bg-red-500 w-2 h-2 my-auto rounded-full animate-pulse"
-                        />
-
-                        <span class="font-bold text-xs dark:text-gray-600 ml-auto mr-0">{{ instance.uuid }}</span>
-
-                        <div class="flex mt-4">
-                          <span class="text-xs dark:text-gray-500">
-                            {{ instance.ip }}
-                          </span>
-
-                          <Icon
-                            name="ion:copy"
-                            class="w-3 h-3 ml-3 dark:text-gray-300 my-auto hover:cursor-pointer"
-                          />
-                        </div>
-                      </div>
-                    </template>
+                    </div>
                   </template>
                 </template>
-              </div>
+              </template>
             </div>
           </div>
         </div>
-        <div v-else>
-          No authorized instances found for this user
-        </div>
-      </client-only>
+      </div>
+      <div v-else>
+        No authorized instances found for this user
+      </div>
     </div>
 
     <div>
