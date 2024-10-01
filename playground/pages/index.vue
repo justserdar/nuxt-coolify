33593c66680f930a8742770c04a30ac3433958c6
h+ <serdar@justserdar.dev>
2024-10-01 13:17:36 +0200

    fix: duplicate v-for names

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -30,93 +30,98 @@
                 v-for="(instanceGroup, instanceGroupIndex) in serverGroup"
                 :key="instanceGroupIndex"
               >
-      <client-only>
-        <div v-if="cPending">
-          Loading Instances...
-        </div>
-        <div v-else-if="cError">
-          Error: {{ cError?.message }}
-        </div>
-        <div v-else-if="!cPending">
-          <ul style="list-style: none;">
-            <li>
-              Total Instances: {{ instances?.length }}
-            </li>
-            <li>
-              <button @click.prevent="refreshInstanceList">
-                Refresh Servers
-              </button>
-            </li>
-          </ul>
-          <div class="grid grid-cols-3 gap-3 py-3">
-            <div
-              v-for="(serverGroup, groupIndex) in instances"
-              :key="groupIndex"
-              class="bg-gray-200 dark:bg-gray-900 p-2 rounded-lg shadow-md"
-            >
-              <div class="grid grid-cols-2 gap-3 space-y-1 px-3 py-4">
-                <template
-                  v-for="instance in instanceGroup"
-                  :key="instance.uuid"
-                >
-                  <template v-if="instance.settings.id === 1">
-                    <p
-                      class="col-span-2"
-                    >
-                      Instance:
-                    </p>
-
-                    <div class="col-span-2 p-6 border rounded hover:cursor-pointer dark:hover:bg-gray-400">
-                      <p
-                        class="flex text-md w-full"
-                        @click="navigateTo(`/hub/s/${instance.uuid}`)"
+                <client-only>
+                  <div v-if="cPending">
+                    Loading Instances...
+                  </div>
+                  <div v-else-if="cError">
+                    Error: {{ cError?.message }}
+                  </div>
+                  <div v-else-if="!cPending">
+                    <ul style="list-style: none;">
+                      <li>
+                        Total Instances: {{ instances?.length }}
+                      </li>
+                      <li>
+                        <button @click.prevent="refreshInstanceList">
+                          Refresh Servers
+                        </button>
+                      </li>
+                    </ul>
+                    <div class="grid grid-cols-3 gap-3 py-3">
+                      <div
+                        v-for="(server, serverIndex) in instances"
+                        :key="serverIndex"
+                        class="bg-gray-200 dark:bg-gray-900 p-2 rounded-lg shadow-md"
                       >
-                        <span class="font-bold text-xs mr-2">{{ instance.name }}</span>
-                      </p>
-                      <div
-                        v-if="instance.settings.is_reachable"
-                        class=" bg-green-500 w-2 h-2 my-auto rounded-full animate-pulse"
-                      />
-                      <div
-                        v-else
-                        class=" bg-red-500 w-2 h-2 my-auto rounded-full animate-pulse"
-                      />
-                      <span class="font-bold text-xs dark:text-gray-600 ml-auto mr-0">{{ instance.uuid }}</span>
-                    </div>
-                  </template>
-                  <template v-else>
-                    <p
-                      class="col-span-2"
-                    >
-                      Attached Servers:
-                    </p>
-                    <div class="col-span-1 p-6 border rounded">
-                      <p
-                        class="flex text-md"
-                        @click="navigateTo(`/hub/s/${instance.uuid}`)"
-                      >
-                        <span class="font-bold text-xs mr-2">{{ instance.name }}</span>
-                      </p>
+                        <div class="grid grid-cols-2 gap-3 space-y-1 px-3 py-4">
+                          <template
+                            v-for="instance in instanceGroup"
+                            :key="instance.uuid"
+                          >
+                            <template v-if="instance.settings.id === 1">
+                              <p
+                                class="col-span-2"
+                              >
+                                Instance:
+                              </p>
 
-                      <div
-                        v-if="instance.settings.is_reachable"
-                        class=" bg-green-500 w-2 h-2 my-auto rounded-full animate-pulse"
-                      />
-                      <div
-                        v-else
-                        class=" bg-red-500 w-2 h-2 my-auto rounded-full animate-pulse"
-                      />
+                              <div class="col-span-2 p-6 border rounded hover:cursor-pointer dark:hover:bg-gray-400">
+                                <p
+                                  class="flex text-md w-full"
+                                  @click="navigateTo(`/hub/s/${instance.uuid}`)"
+                                >
+                                  <span class="font-bold text-xs mr-2">{{ instance.name }}</span>
+                                </p>
+                                <div
+                                  v-if="instance.settings.is_reachable"
+                                  class=" bg-green-500 w-2 h-2 my-auto rounded-full animate-pulse"
+                                />
+                                <div
+                                  v-else
+                                  class=" bg-red-500 w-2 h-2 my-auto rounded-full animate-pulse"
+                                />
+                                <span class="font-bold text-xs dark:text-gray-600 ml-auto mr-0">{{ instance.uuid }}</span>
+                              </div>
+                            </template>
+                            <template v-else>
+                              <p
+                                class="col-span-2"
+                              >
+                                Attached Servers:
+                              </p>
+                              <div class="col-span-1 p-6 border rounded">
+                                <p
+                                  class="flex text-md"
+                                  @click="navigateTo(`/hub/s/${instance.uuid}`)"
+                                >
+                                  <span class="font-bold text-xs mr-2">{{ instance.name }}</span>
+                                </p>
 
-                      <span class="font-bold text-xs dark:text-gray-600 ml-auto mr-0">{{ instance.uuid }}</span>
+                                <div
+                                  v-if="instance.settings.is_reachable"
+                                  class=" bg-green-500 w-2 h-2 my-auto rounded-full animate-pulse"
+                                />
+                                <div
+                                  v-else
+                                  class=" bg-red-500 w-2 h-2 my-auto rounded-full animate-pulse"
+                                />
 
-                      <div class="flex mt-4">
-                        <span class="text-xs dark:text-gray-500">
-                          {{ instance.ip }}
-                        </span>
+                                <span class="font-bold text-xs dark:text-gray-600 ml-auto mr-0">{{ instance.uuid }}</span>
+
+                                <div class="flex mt-4">
+                                  <span class="text-xs dark:text-gray-500">
+                                    {{ instance.ip }}
+                                  </span>
+                                </div>
+                              </div>
+                            </template>
+                          </template>
+                        </div>
                       </div>
                     </div>
-                  </template>
-                </template>
+                  </div>
+                </client-only>
               </template>
             </div>
           </div>
@@ -128,7 +133,7 @@
     </div>
 
     <div>
-      <h2>Hetzner Servers</h2>a
+      <h2>Hetzner Servers</h2>
       <div v-if="hStatus === 'pending'">
         Loading Servers...
       </div>
@@ -196,7 +201,7 @@
           </template>
         </div>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
